69d6f6628a78fca95cd391360bd36437ef77c88a
masahirog <masahiro11g@gmail.com>
2017-10-10 17:39:38 +0900

    jsコメントアウト

--- app/assets/javascripts/order_create.js
+++ app/assets/javascripts/order_create.js
@@ -1,8 +1,8 @@
 $(function(){
-    $('.input_select_product').select2({
-    width:"300px",
-    placeholder: "お弁当を選択してください"
-  });
+  //   $('.input_select_product').select2({
+  //   width:"300px",
+  //   placeholder: "お弁当を選択してください"
+  // });
     $('.select_order_materials').select2({
     width:"200px",
     placeholder: "発注する食材を選択"
