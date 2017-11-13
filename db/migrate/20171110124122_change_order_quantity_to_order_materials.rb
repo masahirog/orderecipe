3bd9fbf56c47677446c0c85e47cf849ec760c9a6
masahirog <masahiro11g@gmail.com>
2017-11-13 20:04:05 +0900

    calculated_quantityをテキスト型に変更

---
+++ db/migrate/20171110124122_change_order_quantity_to_order_materials.rb
@@ -0,0 +1,11 @@
+class ChangeOrderQuantityToOrderMaterials < ActiveRecord::Migration[5.0]
+  #変更後の型
+  def up
+    change_column :order_materials, :order_quantity, :text
+  end
+
+  #変更前の型
+  def down
+    change_column :order_materials, :order_quantity, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171110103452) do
+ActiveRecord::Schema.define(version: 20171110124122) do
 
   create_table "materials", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -53,9 +53,9 @@ ActiveRecord::Schema.define(version: 20171110103452) do
   create_table "order_materials", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "order_id"
     t.integer  "material_id"
-    t.integer  "order_quantity"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.text     "order_quantity",      limit: 65535
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
     t.float    "calculated_quantity", limit: 24
   end
 
