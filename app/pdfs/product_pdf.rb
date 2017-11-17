72b20b24c8ad10f5d4751c8fb7e715e0535253c2
masahirog <masahiro11g@gmail.com>
2017-11-17 11:01:15 +0900

    pdfのデザイン変更

--- app/pdfs/product_pdf.rb
+++ app/pdfs/product_pdf.rb
@@ -11,8 +11,8 @@ class ProductPdf < Prawn::Document
     product = product
     menus = menus
     header_table(product,num)
-    table_content(menus)
-    table_right(menus,num)
+    table_content(menus,num)
+    # table_right(menus,num)
   end
 
 
@@ -35,67 +35,49 @@ class ProductPdf < Prawn::Document
     end
   end
 
-  def table_content(menus)
-    bounding_box([0, 490], :width => 695) do
-    # tableメソッドは2次元配列を引数(line_item_rows)にとり、それをテーブルとして表示する
-    # ブロック(do...end)内でテーブルの書式の設定をしている
-      table line_item_rows(menus), cell_style: { size: 9 } do
+  def table_content(menus,num)
+    bounding_box([0, 490], :width => 765) do
+      table line_item_rows(menus,num) do
       cells.padding = 2
-      cells.borders = [:bottom] # 表示するボーダーの向き(top, bottom, right, leftがある)
+      cells.borders = [:bottom]
       cells.border_width = 0.2
-      cells.height = 14
       column(-2..-1).align = :right
       row(0).border_width = 1
+      row(0).size = 9
       self.header = true
-      self.column_widths = [100,100,100,120,60,100,70,40]
+      self.column_widths = [100,100,100,120,60,100,70,40,5,65]
       end
     end
   end
-  def line_item_rows(menus)
-    data= [["メニュー名","調理メモ","盛付メモ","食材・資材",{:content => "仕込み内容", :colspan => 2},"1人分","使用原価"]]
+  def line_item_rows(menus,num)
+    data= [["メニュー名","調理メモ","盛付メモ","食材・資材",{:content => "仕込み内容", :colspan => 2},"1人分","使用原価","","#{num}人分"]]
     menus.each do |menu|
       u = menu.materials.length
+      recipe_mozi = menu.recipe.length
+      if recipe_mozi<50
+        recipe_size = 9
+      elsif recipe_mozi<100
+        recipe_size = 7
+      else
+        recipe_size = 6
+      end
       menu.menu_materials.each_with_index do |mm,i|
         if i == 0
-          data << [{:content => "#{menu.name}", :rowspan => u},{:content => "#{menu.recipe}", :rowspan => u},
-            {:content => "#{menu.serving_memo}", :rowspan => u},"#{Material.find(mm.material_id).name}","#{mm.post}","#{mm.preparation}",
-            "#{mm.amount_used} #{Material.find(mm.material_id).calculated_unit}","#{(Material.find(mm.material_id).cost_price * mm.amount_used).round(1)}"]
+          data << [{content: "#{menu.name}", rowspan: u,size: 9},
+            {content: "#{menu.recipe}", rowspan: u, size: recipe_size},
+            {content: "#{menu.serving_memo}", rowspan: u, size: 9},{content:"#{Material.find(mm.material_id).name}", size: 9},{content: "#{mm.post}", size: 9},{content:"#{mm.preparation}", size: 9},
+            {content:"#{mm.amount_used} #{Material.find(mm.material_id).calculated_unit}", size: 9},{content:"#{(Material.find(mm.material_id).cost_price * mm.amount_used).round(1)}", size: 9},"",
+            {content:"#{((mm.amount_used * num.to_i).round).to_s(:delimited)} #{Material.find(mm.material_id).calculated_unit}",size:9}]
         else
-          data << ["#{Material.find(mm.material_id).name}","#{mm.post}","#{mm.preparation}","#{mm.amount_used} #{Material.find(mm.material_id).calculated_unit}","#{(Material.find(mm.material_id).cost_price * mm.amount_used).round(1)}"]
+          data << [{content:"#{Material.find(mm.material_id).name}", size: 9},{content:"#{mm.post}", size: 9},{content:"#{mm.preparation}", size: 9},
+            {content:"#{mm.amount_used} #{Material.find(mm.material_id).calculated_unit}", size: 9},{content:"#{(Material.find(mm.material_id).cost_price * mm.amount_used).round(1)}", size: 9},"",
+          {content:"#{((mm.amount_used * num.to_i).round).to_s(:delimited)} #{Material.find(mm.material_id).calculated_unit}",size:9}]
         end
       end
     end
     data
   end
 
-
-  def table_right(menus,num)
-    bounding_box([700, 490], :width => 65) do
-      table right_item_rows(menus,num), cell_style: { size: 9,align: :right } do
-      cells.padding = 2
-      cells.borders = [:bottom,]
-      cells.border_width = 0.2
-      cells.height = 14
-      row(0).border_width = 1
-      self.header     = true
-      self.column_widths = [65]
-      end
-    end
-  end
-  def right_item_rows(menus,num)
-    data= [["#{num}人分"]]
-    menus.each do |menu|
-      u = menu.materials.length
-      menu.menu_materials.each_with_index do |mm,i|
-        if i == 0
-          data << ["#{((mm.amount_used * num.to_i).round).to_s(:delimited)} #{Material.find(mm.material_id).calculated_unit}"]
-        else
-          data << ["#{((mm.amount_used * num.to_i).round).to_s(:delimited)} #{Material.find(mm.material_id).calculated_unit}"]
-        end
-      end
-    end
-    data
-  end
 
   def sen
     stroke_axis
