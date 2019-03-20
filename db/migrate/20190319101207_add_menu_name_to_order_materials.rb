a3c449c6631af4c633b50f618fc21a10364ac395
masahiro <masahiro11g@gmail.com>
2019-03-20 22:27:17 +0900

    no message

--- db/migrate/20190319101207_add_menu_name_to_order_materials.rb
+++ db/migrate/20190319101207_add_menu_name_to_order_materials.rb
@@ -1,7 +1,7 @@
 class AddMenuNameToOrderMaterials < ActiveRecord::Migration[5.2]
   def change
     add_column :order_materials, :menu_name, :text
-    add_column :order_materials, :calculated_unit, :string
-    add_column :order_materials, :order_unit, :string
+    # add_column :order_materials, :calculated_unit, :string
+    # add_column :order_materials, :order_unit, :string
   end
 end
