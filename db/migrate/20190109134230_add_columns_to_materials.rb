48cdc25c80cae98f096e5f97c8e777b4cc36e0e0
masahiro <masahiro11g@gmail.com>
2019-03-20 22:34:24 +0900

    no message

--- db/migrate/20190109134230_add_columns_to_materials.rb
+++ db/migrate/20190109134230_add_columns_to_materials.rb
@@ -1,7 +1,7 @@
 class AddColumnsToMaterials < ActiveRecord::Migration[5.0]
   def change
-    add_column :materials, :vendorstock_flag, :boolean
-    # add_column :materials, :delivery_deadline, :integer, :null => false, :default => 1
+    add_column :materials, :vendor_stock_flag, :boolean, :null => false, :default => true
+    add_column :materials, :delivery_deadline, :integer, :null => false, :default => 1
 
   end
 end
