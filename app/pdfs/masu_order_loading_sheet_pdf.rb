91b141c1b1f336560817e2b2d26d5836156de832
masahiro <masahiro11g@gmail.com>
2019-05-20 07:26:32 +0900

    no message

--- app/pdfs/masu_order_loading_sheet_pdf.rb
+++ app/pdfs/masu_order_loading_sheet_pdf.rb
@@ -13,7 +13,7 @@ class MasuOrderLoadingSheetPdf < Prawn::Document
     masu_orders = MasuOrder.where(start_time:date)
     total = products_num_h.values.inject(:+)
 
-    bounding_box([20, 550], :width => 740) do
+    bounding_box([20, 550], :width => 840) do
       table line_item_rows2(products_num_h,date,masu_orders,total) do
         row(0).background_color = 'f5f5f5'
         cells.padding = [8,6,8,6]
@@ -22,7 +22,7 @@ class MasuOrderLoadingSheetPdf < Prawn::Document
         cells.valign = :center
         columns(2..-1).align = :center
         self.header = true
-        columns = Array.new(masu_orders.length){50}
+        columns = Array.new(masu_orders.length){60}
         self.column_widths = [280,130].push(columns).flatten!
       end
     end
@@ -40,6 +40,11 @@ class MasuOrderLoadingSheetPdf < Prawn::Document
       else
         miso = ''
       end
+      if mo.trash_bags == 0
+        trash_bags = ""
+      else
+        trash_bags = mo.trash_bags
+      end
       if mo.payment == '請求書'
         seikyusho = "◯"
         ryoshusho = ""
@@ -54,7 +59,7 @@ class MasuOrderLoadingSheetPdf < Prawn::Document
       else
         tea = "缶：#{mo.number}"
       end
-      hash2.store(mo.id,[mo.number,mo.number,miso,miso,tea,'◯','◯',seikyusho,ryoshusho])
+      hash2.store(mo.id,[mo.number,mo.number,miso,miso,tea,trash_bags,'◯','◯',seikyusho,ryoshusho])
     end
 
     kurumesi_ids = masu_orders.map{|masu_order| masu_order.kurumesi_order_id}
@@ -72,7 +77,7 @@ class MasuOrderLoadingSheetPdf < Prawn::Document
       data << arr.map {|e| e ? e : ''}
     end
 
-    [['おしぼり','オシボリ'],['お箸','オハシ'],['みそ汁','ミソシル'],['紙コップ','カミコップ'],['お茶','オチャ'],['保冷剤','ホレイザイ'],['納品書','ノウヒンショ'],['請求書','セイキュウショ'],['領収書','リョウシュウショ']].each_with_index do |yoso,i|
+    [['おしぼり','オシボリ'],['お箸','オハシ'],['みそ汁','ミソシル'],['紙コップ','カミコップ'],['お茶','オチャ'],['保冷剤','ホレイザイ'],['ごみ袋','ゴミブクロ'],['納品書','ノウヒンショ'],['請求書','セイキュウショ'],['領収書','リョウシュウショ']].each_with_index do |yoso,i|
       arr = [yoso[0],yoso[1]]
       masu_orders.each do |masu_order|
         arr.push(hash2[masu_order.id][i])
