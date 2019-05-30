1820a53fbb5915aa1322ea6db3974589fb143545
masahiro <masahiro11g@gmail.com>
2019-05-30 16:51:38 +0900

    no message

--- app/controllers/incoming_emails_controller.rb
+++ app/controllers/incoming_emails_controller.rb
@@ -5,7 +5,7 @@ class IncomingEmailsController < ApplicationController
   def create
     mail = Mail.new(params[:message])
 
-    @kurumesi_mail = KurumesiMail.create(message:mail.body)
+    @kurumesi_mail = KurumesiMail.create(message:'aaa')
     # ここに保存＆ブラウザー通知
     render text: "OK"
   end
