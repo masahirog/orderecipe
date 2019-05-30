0aa9a4b753a5e55a126e64aa0c9e9827680ad658
masahiro <masahiro11g@gmail.com>
2019-05-30 16:23:16 +0900

    no message

--- app/controllers/incoming_emails_controller.rb
+++ app/controllers/incoming_emails_controller.rb
@@ -2,7 +2,6 @@ require 'mail'
 require 'kconv'
 
 class IncomingEmailsController < ApplicationController
-  skip_before_filter :verify_authenticity_token, only: :test
   def test
     mail = KurumesiMail.create(message:params[:message])
     # ここに保存＆ブラウザー通知
