fe499f3f74d7ed149c6ec4f98261676c9dbca932
masahiro <masahiro11g@gmail.com>
2019-05-31 17:40:49 +0900

    no message

--- app/controllers/incoming_emails_controller.rb
+++ app/controllers/incoming_emails_controller.rb
@@ -1,14 +1,14 @@
 require 'mail'
 
 class IncomingEmailsController < ActionController::Base
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
   def create
     mail = Mail.new(params[:message])
     # TODO: use 'mail' object
     # see API for https://github.com/mikel/mail
     @kurumesi_mail = KurumesiMail.new(message:'aaa')
     @kurumesi_mail.save
-    
+
     # render text: "OK"
   end
 end
