03e628f58135e6b815dc120d17a87953977da4ec
masahiro <masahiro11g@gmail.com>
2019-09-21 12:31:53 +0900

    no message

--- db/migrate/20190920055200_create_cooking_rices.rb
+++ db/migrate/20190920055200_create_cooking_rices.rb
@@ -4,7 +4,7 @@ class CreateCookingRices < ActiveRecord::Migration[5.2]
       t.string :name,null:false
       t.integer :base_rice,null:false
       t.integer :serving_amount,null:false
-      t.integer :sho_per_make_num,null:false
+      t.float :sho_per_make_num,null:false
 
       t.timestamps
     end
