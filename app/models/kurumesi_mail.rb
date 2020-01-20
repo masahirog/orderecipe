bb80de3987e939f430935e667a3453f9809a3282
masahiro <masahiro11g@gmail.com>
2020-01-20 20:23:53 +0900

    no message

--- app/models/kurumesi_mail.rb
+++ app/models/kurumesi_mail.rb
@@ -31,7 +31,7 @@ class KurumesiMail < ApplicationRecord
     # 'FROM','info@kurumesi-bentou.com',
     search_criterias = [
       'FROM','info@kurumesi-bentou.com',
-      'SINCE', (Date.today-3).strftime("%d-%b-%Y")
+      'SINCE', (Date.today).strftime("%d-%b-%Y")
     ]
     imap.select('INBOX') # 対象のメールボックスを選択
     ids = imap.search(search_criterias) # 全てのメールを取得
