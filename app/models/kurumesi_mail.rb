cd67f3fc19b6ffc070e970befcdbe2abd362cb94
masahiro <masahiro11g@gmail.com>
2020-07-29 23:08:46 +0900

    no message

--- app/models/kurumesi_mail.rb
+++ app/models/kurumesi_mail.rb
@@ -25,12 +25,12 @@ class KurumesiMail < ApplicationRecord
     imap_port = 993 # ssl有効なら993、そうでなければ143
     imap = Net::IMAP.new(imap_host, imap_port, imap_usessl)
     # imapにログイン
-    imap_user = 'masahiro11g@gmail.com'
-    imap_passwd = ENV['GMAIL_PASS']
+    imap_user = 'd@bento.jp'
+    imap_passwd = ENV['D_BENTO_PASS']
     imap.login(imap_user, imap_passwd)
     # 'FROM','info@kurumesi-bentou.com',
     search_criterias = [
-      'FROM','info@kurumesi-bentou.com',
+      'FROM','masahiro11g@gmail.com',
       'SINCE', (Date.today-1).strftime("%d-%b-%Y")
       # 'SINCE', (Date.today).strftime("%d-%b-%Y") テスト環境
     ]
