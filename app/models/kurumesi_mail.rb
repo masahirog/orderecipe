f7032d2149e1a1097cf30860bbcdff9f8dbda93c
masahiro <masahiro11g@gmail.com>
2021-01-05 18:20:03 +0900

    no message

--- app/models/kurumesi_mail.rb
+++ app/models/kurumesi_mail.rb
@@ -25,8 +25,8 @@ class KurumesiMail < ApplicationRecord
     imap_port = 993 # ssl有効なら993、そうでなければ143
     imap = Net::IMAP.new(imap_host, imap_port, imap_usessl)
     # imapにログイン
-    imap_user = 'd@bento.jp'
-    imap_passwd = ENV['D_BENTO_PASS']
+    imap_user = 'masahiro11g@gmail.com'
+    imap_passwd = ENV['GMAIL_PASS']
     imap.login(imap_user, imap_passwd)
     # 'FROM','info@kurumesi-bentou.com',
     search_criterias = [
