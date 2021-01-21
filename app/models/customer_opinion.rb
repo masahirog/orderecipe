1faf89f95e78a2440acf023fb68803c4c2eae41e
masahiro <masahiro11g@gmail.com>
2021-01-21 14:39:55 +0900

    no message

--- app/models/customer_opinion.rb
+++ app/models/customer_opinion.rb
@@ -1,6 +1,6 @@
 class CustomerOpinion < ApplicationRecord
   def self.post_shere(customer_opinion)
-    line_notify = LineNotify.new('TQe4fwSwkJ7hNvtAsI00MDCxNnKC3QmHtYesQUPmJqJ')
+    line_notify = LineNotify.new('V77tCISPuOwwxsSWTZEBHJjonL4LH1PBwVVEaXXlx9F')
     options = {
       message: "\n#{customer_opinion.content}"
     }
