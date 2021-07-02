cb4cc5fb143b2870035808993509b73f72baa431
masahirog <masahiro11g@gmail.com>
2021-07-02 10:12:36 +0900

    no message

--- app/models/kurumesi_mail.rb
+++ app/models/kurumesi_mail.rb
@@ -25,13 +25,13 @@ class KurumesiMail < ApplicationRecord
     imap_port = 993 # ssl有効なら993、そうでなければ143
     imap = Net::IMAP.new(imap_host, imap_port, imap_usessl)
     # imapにログイン
-    imap_user = 'masahiro11g@gmail.com'
-    imap_passwd = ENV['MASA_MAIL_PASS']
+    imap_user = 'm.yamashita@jfd.co.jp'
+    imap_passwd = ENV['MASA_MAIL_PASS']'
     imap.login(imap_user, imap_passwd)
     # 'FROM','info@kurumesi-bentou.com',
     search_criterias = [
       'FROM','info@kurumesi-bentou.com',
-      'SINCE', (Date.today-1).strftime("%d-%b-%Y")
+      'SINCE', (Date.today-3).strftime("%d-%b-%Y")
       # 'SINCE', (Date.today).strftime("%d-%b-%Y") テスト環境
     ]
     imap.select('INBOX') # 対象のメールボックスを選択
