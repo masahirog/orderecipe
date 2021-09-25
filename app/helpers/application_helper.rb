7a266a4fbdc4f7a7d0ead0f8dbe9afbe9c88014d
masahirog <masahiro11g@gmail.com>
2021-09-25 23:52:26 +0900

    no message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -58,7 +58,7 @@ module ApplicationHelper
     #Enum型のハッシュリストに対して、nameと日本語化文字列の配列を取得（valueは使わないため_)
     enum_list.map do | name , _value |
       # selectで使うための組み合わせ順は[ 表示値, value値 ]のため以下の通り設定
-      [ t("enums.#{klass.to_s.downcase}.#{column}.#{name}") , name ]
+      [ t("enums.#{klass.to_s.underscore}.#{column}.#{name}") , name ]
     end
   end
 end
