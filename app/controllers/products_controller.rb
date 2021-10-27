a83980a30cdb9bc2ce36cc18460ca98383f4955f
masahirog <masahiro11g@gmail.com>
2021-10-27 09:00:29 +0900

    no message

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -289,7 +289,7 @@ class ProductsController < AdminController
       params.require(:product).permit(:name,:memo, :management_id,:short_name,:symbol, :sell_price, :description, :contents, :image,:brand_id,:product_category,:bejihan_sozai_flag,
                       :food_label_name,:food_label_content,:status,:remove_image, :image_cache,:display_image,:image_for_one_person,:serving_infomation,:carryover_able_flag,
                       :main_serving_plate_id,:sub_serving_plate_id,:container_id,:ozara_serving_infomation,
-                      :cost_price,:cooking_rice_id, product_menus_attributes: [:id, :product_id, :menu_id,:row_order, :_destroy],product_pops_attributes: [:id, :product_id,:image],
+                      :cost_price,:cooking_rice_id, product_menus_attributes: [:id, :product_id, :menu_id,:row_order, :_destroy],product_pops_attributes: [:id, :product_id,:image,:remove_image,:image_cache],
                     product_parts_attributes: [:id,:product_id,:name,:amount,:unit, :_destroy,:memo,:container],
                     product_ozara_serving_informations_attributes: [:id, :product_id,:row_order,:content,:image, :_destroy])
     end
