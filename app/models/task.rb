e239e865ad8ec16c3940e08f4a76dccf74679476
masahirog <masahiro11g@gmail.com>
2022-06-11 12:45:31 +0900

    no message

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,8 @@
 class Task < ApplicationRecord
-  has_many :task_comments
-  enum status: {todo:0,doing:1,checking:2,done:3}
+  has_many :task_comments,dependent: :destroy
   has_many :task_staffs, dependent: :destroy
   accepts_nested_attributes_for :task_staffs, allow_destroy: true
+  enum status: {todo:0,doing:1,checking:2,done:3}
   include RankedModel
   ranks :row_order, :with_same => :status
 end
