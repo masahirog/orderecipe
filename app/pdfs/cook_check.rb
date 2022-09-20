147dffd87200de570eb130419ad575716890d044
masahirog <masahiro11g@gmail.com>
2022-09-20 13:41:11 +0900

    no message

--- app/pdfs/cook_check.rb
+++ app/pdfs/cook_check.rb
@@ -7,7 +7,7 @@ class CookCheck < Prawn::Document
     #日本語のフォント
     font "vendor/assets/fonts/ipaexg.ttf"
     daily_menu = DailyMenu.find(daily_menu_id)
-    bounding_box([10, 800], :width => 550) do
+    bounding_box([10, 800], :width => 560) do
       text "発行時間：#{Time.now.strftime("%Y年 %m月 %d日　%H:%M")}",size:8,:align => :right
       text "#{daily_menu.start_time.strftime("%-m/%-d (#{%w(日 月 火 水 木 金 土)[daily_menu.start_time.wday]})")} 献立"
       move_down 10
@@ -25,17 +25,21 @@ class CookCheck < Prawn::Document
       cells.border_width = 0.1
       # cells.valign = :center
       self.header = true
-      self.column_widths = [150,300,100]
+      self.column_widths = [150,130,200,80]
     end
   end
 
   def line_item_rows(daily_menu)
-    data = [['メニュー名','チェック内容','チェック者']]
+    data = [['商品名','メニュー名','チェック内容','チェック者']]
     daily_menu.daily_menu_details.each_with_index do |dmd,i|
-      dmd.product.menus.each do |menu|
-        menu.menu_cook_checks.each do |mcc|
-          data << [menu.name,mcc.content,'']
+      if MenuCookCheck.exists?(menu_id:dmd.product.menus.ids)
+        dmd.product.menus.each do |menu|
+          menu.menu_cook_checks.each do |mcc|
+            data << [dmd.product.name,menu.name,mcc.content,'']
+          end
         end
+      else
+        data << [dmd.product.name,'','','']
       end
     end
     data
