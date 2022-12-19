e5c3d3d443363484d707d2586f247b0b5a7ad12c
masahirog <masahiro11g@gmail.com>
2022-12-20 00:19:24 +0900

    no message

--- app/models/product_part.rb
+++ app/models/product_part.rb
@@ -1,4 +1,4 @@
 class ProductPart < ApplicationRecord
   belongs_to :product
-  enum container: {ビニル袋:0, 真空パック:1,タッパー:2,バット:3,カップ:4}
+  enum container: {ビニル袋:0, 真空パック:1,タッパー:2,バット:3,カップ:4,パック（シーラー）:5}
 end
