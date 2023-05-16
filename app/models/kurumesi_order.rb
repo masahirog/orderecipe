f7a678cc981cb661defb159165e04ad3b4c6a28f
masahirog <masahiro11g@gmail.com>
2023-05-16 13:55:29 +0900

    no message

--- app/models/kurumesi_order.rb
+++ app/models/kurumesi_order.rb
@@ -110,7 +110,7 @@ class KurumesiOrder < ApplicationRecord
       if dmf.present?
         if num == dmf.num
         else
-          if num > 130
+          if num > 150
             if dmf.notified_flag == true
               dmf.num = num
             else
@@ -124,7 +124,7 @@ class KurumesiOrder < ApplicationRecord
           update_arr << dmf
         end
       else
-        if num > 130
+        if num > 150
           new_arr << DateManufactureNumber.new(date:date,num:num,notified_flag:true)
           capacity_notify(date,num)
         else
