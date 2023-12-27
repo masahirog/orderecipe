0b44bc98f12d60949f511445ace708ca58493823
masahirog <masahiro11g@gmail.com>
2023-12-27 18:11:54 +0900

    no message

--- db/schema.rb
+++ db/schema.rb
@@ -82,7 +82,6 @@ ActiveRecord::Schema.define(version: 2023_12_26_074422) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.string "store_path"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "group_id", null: false
