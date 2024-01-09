d5d506d7536d7635ce5a29677bffb451d510dfda
masahirog <masahiro11g@gmail.com>
2024-01-09 19:50:55 +0900

    no message

--- app/pdfs/price_card.rb
+++ app/pdfs/price_card.rb
@@ -13,16 +13,7 @@ class PriceCard < Prawn::Document
     products.each_with_index do |product,i|
       if product.image.present?
         stroke do
-          # if i == 0
-          #   font "vendor/assets/fonts/ipaexg.ttf"
-          # elsif i == 1
-          #   font "vendor/assets/fonts/ZenMaruGothic-Medium.ttf"
-          # elsif i == 2
-          #   font "vendor/assets/fonts/ipaexm.ttf"
-          # elsif i == 3
-          #   font "vendor/assets/fonts/ZenKakuGothicNew-Medium.ttf"
-          # end
-          font "vendor/assets/fonts/ipaexm.ttf"
+          font "vendor/assets/fonts/NotoSansJP-Black.ttf"
           # fill_color 'ffffff'
           image open(product.image.url), at: [10, h], width:184
           fill_color '000000'
@@ -30,38 +21,38 @@ class PriceCard < Prawn::Document
           stroke_color 'ececec'
           stroke_rectangle [10,h], 510, 184
           fill_color '000000'
-          text_box(product.food_label_name,at: [215,h-18], width: 220, height: 50,size:18,valign: :center, leading: 3)
-          line_width 1
-          line [210, h-75], [500, h-75]
+          text_box(product.food_label_name,at: [215,h-45], width: 280, height: 50,size:15,valign: :center, leading: 3,align: :center)
+          line_width 2
+          line [210, h-95], [500, h-95]
           stroke_color '000000'
           stroke
 
-          text_box(product.contents,at: [215,h-80], width: 280, height: 20,size:8,valign: :center)
-
-
           fill_color '000000'
-          text_box("<font size='30'>#{product.sell_price}</font>円",at: [355, h - 135], width: 100, height: 40,size:16,inline_format: true)
-          text_box("1人前",at: [440, h - 138], width: 100, height: 40,size:11)
-          text_box("（税込 #{product.tax_including_sell_price} 円）",at: [430, h - 153], width: 100, height: 40,size:10)
+          text_box("<font size='22'>#{product.sell_price}</font>円",at: [330, h - 135], width: 100, height: 40,size:16,inline_format: true)
 
-          font "vendor/assets/fonts/ipaexg.ttf"
+
+          font "vendor/assets/fonts/NotoSansJP-Medium.ttf"
+          text_box(product.contents,at: [215,h-100], width: 280, height: 20,size:8,valign: :center,align: :center, leading: 3)
+          text_box("一人前",at: [294, h - 143], width: 100, height: 40,size:10)
+          text_box("（税込 #{product.tax_including_sell_price} 円）",at: [315, h - 160], width: 100, height: 40,size:9)
+
           if product.product_category == "スイーツ・ドリンク"
             fill_color 'F2684A'
-            fill_rounded_rectangle [215,h-130], 36, 36, 18
+            fill_rounded_rectangle [415,h-10], 36, 36, 18
             fill_color 'ffffff'
-            text_box("甘味",at: [217, h - 143], width: 36, height: 36, rotate_around: :center,align: :center,size:10)
+            text_box("甘味",at: [417, h - 23], width: 36, height: 36, rotate_around: :center,align: :center,size:10)
           else
             fill_color '78B86D'
-            fill_rounded_rectangle [215,h-130], 36, 36, 18
+            fill_rounded_rectangle [415,h-10], 36, 36, 18
             fill_color 'ffffff'
-            text_box("週替り",at: [216, h - 143], width: 36, height: 36, rotate_around: :center,align: :center,size:9)
+            text_box("週替り",at: [416, h - 23], width: 36, height: 36, rotate_around: :center,align: :center,size:9)
           end
 
           if product.warm_flag == true
             fill_color 'ce5242'
-            fill_rounded_rectangle [260,h-130], 36, 36, 18
+            fill_rounded_rectangle [460,h-10], 36, 36, 18
             fill_color 'ffffff'
-            text_box("温め",at: [261, h - 143], width: 36, height: 36, rotate_around: :center,align: :center,size:11)
+            text_box("温め",at: [461, h - 23], width: 36, height: 36, rotate_around: :center,align: :center,size:11)
           end
         end
       else
