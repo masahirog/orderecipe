11bb4428e53666662391964d60ee131e9a915ec3
masahirog <masahiro11g@gmail.com>
2024-03-10 19:17:57 +0900

    no message

--- app/models/product_pop.rb
+++
@@ -1,4 +0,0 @@
-class ProductPop < ApplicationRecord
-  belongs_to :product
-  mount_uploader :image, ProductPopUploader
-end
--- db/migrate/20210316124221_create_product_pops.rb
+++
@@ -1,9 +0,0 @@
-class CreateProductPops < ActiveRecord::Migration[5.2]
-  def change
-    create_table :product_pops do |t|
-      t.integer :product_id,null:false
-      t.string :image
-      t.timestamps
-    end
-  end
-end
