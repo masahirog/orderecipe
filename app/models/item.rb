712ab3885d660093714d31f18b7b2c399132974c
masahirog <masahiro11g@gmail.com>
2024-05-03 17:18:00 +0900

    no message

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
 	# enum category: {野菜:1,果物:2,物産:3,送料:4}
 	enum unit: {袋:1,cs:2,本:3,kg:4,個:5,pc:6,口:7}, _prefix: true
 	enum order_unit: {袋:1,cs:2,本:3,kg:4,個:5,pc:6,口:7}, _prefix: true
-	enum status: {販売中:0,出荷終了:1,販売終了:2}
+	enum status: {販売中:0,"販売中(入荷待ち)":1,販売終了:2,"終了(非表示)":3}
 	def view_name_and_vendor
 		self.name + "｜" +self.item_vendor.store_name
 	end
