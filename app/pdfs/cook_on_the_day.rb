1a7eda4dba4b04640d7de38f46d7605c7d7241a2
masahirog <masahiro11g@gmail.com>
2024-05-12 16:23:55 +0900

    no message

--- app/pdfs/cook_on_the_day.rb
+++ app/pdfs/cook_on_the_day.rb
@@ -25,19 +25,11 @@ class CookOnTheDay < Prawn::Document
           cells.size = 7
           cells.border_width = 0.1
           columns(-5..-4).align = :center
+          columns(3).size = 12
+          rows(0).weight = :bold
+          rows(0).size = 8
           self.header = true
           self.column_widths = [140,90,40,40,130,40,40,40]
-        end
-        move_down 10
-        text "朝調理個数一覧："
-        move_down 5
-        table line_item_rows(sdm) do
-          self.row_colors = ["FFFFFF","E5E5E5"]
-          cells.size = 7
-          cells.border_width = 0.1
-          columns(1..-1).align = :center
-          self.header = true
-          self.column_widths = [180,50,50,100,100]
         end
         move_down 10
         text "その他メモ："
@@ -46,35 +38,17 @@ class CookOnTheDay < Prawn::Document
     end
   end
   def cook_on_the_day(sdm)
-    data = [['商品名','品目','合計','朝数','工程','中心温','調理✓','積載✓']]
+    data = [['商品名','品目','製造数','調理数','工程','中心温','調理✓','積載✓']]
     sdm.store_daily_menu_details.each do |sdmd|
-      if sdmd.product.carryover_able_flag == true
-        number = "※ " + (sdmd.number*0.7).ceil.to_s
+      if sdmd.carry_over > 0
+        morning_number = "※ " + sdmd.actual_inventory.to_s
       else
-        number = sdmd.number
+        morning_number = sdmd.number
       end
       sdmd.product.menus.each do |menu|
         menu.menu_last_processes.each do |mlp|
-          data << [sdmd.product.name,mlp.content,sdmd.number,number,mlp.memo,'','','']
+          data << [sdmd.product.name,mlp.content,sdmd.number,morning_number,mlp.memo,'','','']
         end
-      end
-    end
-    data
-  end
-
-
-  def line_item_rows(sdm)
-    data = [['商品名','翌日繰越','冷凍保存',sdm.store.name,'朝調理分']]
-    sdm.store_daily_menu_details.each do |sdmd|
-      if sdmd.product.carryover_able_flag == true
-        if sdmd.product.freezing_able_flag == true
-          reito = '○'
-        else
-          reito = ''
-        end
-        morning_number = "※　" + (sdmd.number*0.7).ceil.to_s
-        data << [sdmd.product.name,'○',reito,sdmd.number,morning_number]
-      else
       end
     end
     data
