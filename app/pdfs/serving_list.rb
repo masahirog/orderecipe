ce40e202eda34009d4bd8d29c336019926dce4cb
masahirog <masahiro11g@gmail.com>
2024-05-27 12:27:51 +0900

    no message

--- app/pdfs/serving_list.rb
+++ app/pdfs/serving_list.rb
@@ -9,15 +9,20 @@ class ServingList < Prawn::Document
       table line_item_rows(daily_menu) do
         cells.size = 9
         self.header = true
-        self.column_widths = [200,320]
+        self.column_widths = [150,100,270]
       end
     end
   end
   def line_item_rows(daily_menu)
     data= []
-    data << ["品名",'説明']
+    data << ["品名","包材",'説明']
     daily_menu.daily_menu_details.order(:paper_menu_number).each do |dmd|
-      data << [dmd.product.food_label_name,dmd.product.serving_infomation]
+      if dmd.product.container.present?
+        container = dmd.product.container.name
+      else
+        container = ""
+      end
+      data << [dmd.product.food_label_name,container,dmd.product.serving_infomation]
     end
     data
   end
