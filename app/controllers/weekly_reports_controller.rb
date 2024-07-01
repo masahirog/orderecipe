233eb33363b3604300f27c8e9f3f40124b7ce55f
masahirog <masahiro11g@gmail.com>
2024-07-01 20:19:18 +0900

    no message

--- app/controllers/weekly_reports_controller.rb
+++ app/controllers/weekly_reports_controller.rb
@@ -2,7 +2,7 @@ class WeeklyReportsController < ApplicationController
   before_action :set_weekly_report, only: %i[ show edit update destroy ]
 
   def index
-    @weekly_reports = WeeklyReport.all
+    @weekly_reports = WeeklyReport.all.order('id desc')
   end
 
   def show
@@ -28,26 +28,28 @@ class WeeklyReportsController < ApplicationController
     @weekly_report = WeeklyReport.new(weekly_report_params)
     respond_to do |format|
       if @weekly_report.save
-        report=" *予算* ：円\n"+
-        " *memo* ：\n\n"+
+        report=" *スタッフ名* ：#{@weekly_report.staff.name} \n\n"+
+        " *日付* ：#{@weekly_report.date}\n\n"+
+        " *店舗* ：#{@weekly_report.store.short_name}\n\n"+
+        " *P：今週の目標・計画*\n#{@weekly_report.plan}\n\n"+
+        " *D：実行したこと*\n#{@weekly_report.do}\n\n"+
+        " *C：振り返り・結果*\n#{@weekly_report.check}\n\n"+
+        " *A：次週のアクション*\n#{@weekly_report.action}\n\n"+
         "ーーーーー\n"
         if params[:weekly_report]["slack_notify"]=="1"
-          
-          Slack::Notifier.new("https://hooks.slack.com/services/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", username: 'おつかれ様', icon_emoji: ':male-farmer:').ping(report)
-          # Slack::Notifier.new("https://hooks.slack.com/services/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", username: 'おつかれ様', icon_emoji: ':male-farmer:').ping(report)
+          # Slack::Notifier.new("https://hooks.slack.com/services/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", username: 'おつかれ様', icon_emoji: ':male-farmer:').ping(report)
+          Slack::Notifier.new("https://hooks.slack.com/services/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", username: 'おつかれ様', icon_emoji: ':male-farmer:').ping(report)
         end
-
         if @weekly_report.weekly_report_thanks.present?
           @weekly_report.weekly_report_thanks.each do |wrt|
-            kindess_message = "ーーーー\n<@#{wrt.staff.slack_id}> \n#{wrt.thanks}（#{@weekly_report.staff.name} さんから）\nーーーーー"
+            kindess_message = "<@#{wrt.staff.slack_id}> \n#{wrt.thanks}（<@#{@weekly_report.staff.slack_id}> さんから）\nーーーーー"
             if params[:weekly_report]["slack_notify"]=="1"
-              Slack::Notifier.new("https://hooks.slack.com/services/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", username: '感謝', icon_emoji: ':hugging_face:').ping(kindess_message)
-              # Slack::Notifier.new("https://hooks.slack.com/services/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", username: '感謝', icon_emoji: ':hugging_face:').ping(kindess_message)
+              # Slack::Notifier.new("https://hooks.slack.com/services/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", username: '感謝', icon_emoji: ':hugging_face:').ping(kindess_message)
+              Slack::Notifier.new("https://hooks.slack.com/services/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", username: '感謝', icon_emoji: ':hugging_face:').ping(kindess_message)
             end
-
           end
         end
-        format.html { redirect_to weekly_reports_path(store_id:@weekly_report.store_id), success: "保存しました。" }
+        format.html { redirect_to weekly_reports_path, success: "保存しました。" }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -57,11 +59,30 @@ class WeeklyReportsController < ApplicationController
   def update
     respond_to do |format|
       if @weekly_report.update(weekly_report_params)
-        format.html { redirect_to weekly_report_url(@weekly_report), notice: "Weekly report was successfully updated." }
-        format.json { render :show, status: :ok, location: @weekly_report }
+        report=" *スタッフ名* ：#{@weekly_report.staff.name} \n\n"+
+        " *日付* ：#{@weekly_report.date}\n\n"+
+        " *店舗* ：#{@weekly_report.store.short_name}\n\n"+
+        " *P：今週の目標・計画*\n#{@weekly_report.plan}\n\n"+
+        " *D：実行したこと*\n#{@weekly_report.do}\n\n"+
+        " *C：振り返り・結果*\n#{@weekly_report.check}\n\n"+
+        " *A：次週のアクション*\n#{@weekly_report.action}\n\n"+
+        "ーーーーー\n"
+        if params[:weekly_report]["slack_notify"]=="1"
+          # Slack::Notifier.new("https://hooks.slack.com/services/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", username: 'おつかれ様', icon_emoji: ':male-farmer:').ping(report)
+          Slack::Notifier.new("https://hooks.slack.com/services/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", username: 'おつかれ様', icon_emoji: ':male-farmer:').ping(report)
+        end
+        if @weekly_report.weekly_report_thanks.present?
+          @weekly_report.weekly_report_thanks.each do |wrt|
+            kindess_message = "<@#{wrt.staff.slack_id}> \n#{wrt.thanks}（<@#{@weekly_report.staff.slack_id}> さんから）\nーーーーー"
+            if params[:weekly_report]["slack_notify"]=="1"
+              # Slack::Notifier.new("https://hooks.slack.com/services/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", username: '感謝', icon_emoji: ':hugging_face:').ping(kindess_message)
+              Slack::Notifier.new("https://hooks.slack.com/services/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", username: '感謝', icon_emoji: ':hugging_face:').ping(kindess_message)
+            end
+          end
+        end
+        format.html { redirect_to weekly_reports_path, notice: "Weekly report was successfully updated." }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @weekly_report.errors, status: :unprocessable_entity }
       end
     end
   end
