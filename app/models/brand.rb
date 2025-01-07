61f391a5e81dd9d6edf9a27b6532b38bfecddd65
masahirog <masahiro11g@gmail.com>
2025-01-07 20:02:44 +0900

    no message

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,4 +1,5 @@
 class Brand < ApplicationRecord
+  belongs_to :group
   has_many :products
   has_many :reviews
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,12 +1,13 @@
 class Group < ApplicationRecord
-    has_many :staffs
-    has_many :stores
-    has_many :shift_frames
-    has_many :fix_shift_patterns
-    has_many :tasks
-    has_many :materials
-    has_many :menus
-    has_many :products
-    has_many :vendors
-    has_many :users
+    has_many :staffs, dependent: :destroy
+    has_many :stores, dependent: :destroy
+    has_many :shift_frames, dependent: :destroy
+    has_many :fix_shift_patterns, dependent: :destroy
+    has_many :tasks, dependent: :destroy
+    has_many :materials, dependent: :destroy
+    has_many :menus, dependent: :destroy
+    has_many :products, dependent: :destroy
+    has_many :vendors, dependent: :destroy
+    has_many :users, dependent: :destroy
+    has_many :brands, dependent: :destroy
 end
